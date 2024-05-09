bd9bd8b60f96cf10063c08a96fb97d6b411bfca1
kunwar bhagat singh <kunwarbhagatsingh082002@gmail.com>
2024-05-10 01:47:58 +0530

    files created

---
+++ src/data.js
@@ -0,0 +1,67 @@
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: "https://cdn.thecodehelp.in/Agra.jpeg",
+		price: "35,758",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+		price: "62,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://cdn.thecodehelp.in/Goa.jpeg",
+		price: "29,695",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+		price: "31,095",
+	},
+	{
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+		price: "70,595",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+		price: "67,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+		price: "60,595",
+	},
+	{
+		id: 8,
+		name: "Kashmir",
+		info: "Kashmir, Region of the northwestern Indian subcontinent. It is bounded to the northeast and east by China, to the south by India, to the west by Pakistan, and to the northwest by Afghanistan. The land is predominantly mountainous and includes K2 and other peaks of the Karakoram Range.",
+		image: "https://c4.wallpaperflare.com/wallpaper/670/272/895/happy-snow-fall-kashmir-murree-pakistan-wallpaper-preview.jpg",
+		price: "76,595",
+	},
+	{
+		id: 9,
+		name: "Kanyakumari",
+		info: "It is the southernmost tip of the Indian subcontinent and the southernmost city in mainland India, and thus referred to as The Land's End. The city is situated 90 kilometres (56 mi) south of Thiruvananthapuram city, and about 20 kilometres (12 mi) south of Nagercoil, the headquarters of Kanyakumari district.",
+		image: "https://media.istockphoto.com/id/1293613951/photo/kanyakumari-vivekananda-rock-memorial-thiruvalluvar-statue-in-the-evening-with-a-colorful-and.webp?b=1&s=170667a&w=0&k=20&c=ZFKLAy-5nD79b8pJMq8qu_15cT09VqooMS2N0Z_nNDw=",
+		price: "88,595",
+	}
+];
+
+export default data;
